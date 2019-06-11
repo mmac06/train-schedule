79f6a96710aef9c19abf3445b3300313bdd80b2a
Mike McMahon <mikemcmahon@isen-macbook02.local>
2019-06-10 19:08:49 -0500

    linked my JS sheet

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+            // Your web app's Firebase configuration
+            var firebaseConfig = {
+                apiKey: "AIzaSyDbAOdKBJeNNyuxSHBTHJDlva--BDMgN_I",
+                authDomain: "bootcamp-train-station.firebaseapp.com",
+                databaseURL: "https://bootcamp-train-station.firebaseio.com",
+                projectId: "bootcamp-train-station",
+                storageBucket: "bootcamp-train-station.appspot.com",
+                messagingSenderId: "1048074352466",
+                appId: "1:1048074352466:web:0f5108f63c93a3e5"
+            };
+            // Initialize Firebase
+            firebase.initializeApp(firebaseConfig);
+
+            console.log("JAVA TEST!");
